fb61600e085c99ee1f63ff9b33cbe49af11aa8a6
Robert Koch <robert@kochie.io>
2022-11-19 15:04:41 +1100

    added success and failure pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import { ReactNode } from "react";
-import SearchHook from "./SearchHook";
+// import SearchHook from "./SearchHook";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <SearchHook />
+        {/* <SearchHook /> */}
         {children}
       </body>
     </html>
---
+++ app/will-fail/[dynamic]/page.tsx
@@ -0,0 +1,29 @@
+import { copyFile, mkdir } from "fs/promises";
+import Image from "next/image";
+import SearchHook from "../SearchHook";
+
+const DynamicPage = async ({ params }) => {
+  await mkdir("public/images", { recursive: true });
+  await copyFile("assets/test.png", "public/images/test.png");
+
+  return (
+    <div>
+      <h1>DynamicPage Component</h1>
+      <pre>{JSON.stringify(params, null, 2)}</pre>
+      <SearchHook />
+      <Image src="/images/test.png" alt="" />
+    </div>
+  );
+};
+
+export default DynamicPage;
+
+/**
+ * @see https://beta.nextjs.org/docs/api-reference/generate-static-params
+ * @returns
+ */
+export async function generateStaticParams() {
+  return [...Array(1000)].map((_, index) => ({
+    willFail: `page-${index}`,
+  }));
+}
